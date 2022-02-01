638e08e1d8f369f1e99fe23ab939c3c303b995b6
Gabriel Hernández <geht2smart@gmail.com>
2022-02-01 15:08:49 -0400

    Optimizado código

--- JS/teamSlider.js
+++ JS/teamSlider.js
@@ -1,30 +1,27 @@
 const teamMembers = document.getElementsByClassName("member-team");
 var firstMember = 0;
+var lastMember = 3;
 
 function scrollMembersToRight() {
-	var lastMember = firstMember + 3;
-	if (lastMember > 4) {
-		lastMember = firstMember - 2;
+	if (firstMember > 4) {
+		firstMember = 0;
+	} else if (lastMember > 4) {
+		lastMember = 0;
 	}
 	teamMembers.item(firstMember).style.display = "none";
 	teamMembers.item(lastMember).style.display = "block";
-	if (firstMember == 4) {
-		firstMember = 0;
-	} else {
-		firstMember++;
-	}
+	firstMember++;
+	lastMember++;
 }
 
 function scrollMembersToLeft() {
-	var lastMember = firstMember + 3;
-	if (lastMember > 4) {
-		lastMember = firstMember - 2;
+	if (firstMember > 4) {
+		firstMember = 0;
+	} else if (lastMember > 4) {
+		lastMember = 0;
 	}
 	teamMembers.item(firstMember).style.display = "none";
 	teamMembers.item(lastMember).style.display = "block";
-	if (firstMember == 0) {
-		firstMember = 4;
-	} else {
-		firstMember--;
-	}
+	firstMember++;
+	lastMember++;
 }
